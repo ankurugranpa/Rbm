9331b74b4df496aada32a14a0a44312d5e628f6e
t213214 <t213214@yzaidgpus.yzdn.yz.yamagata-u.ac.jp>
2024-09-12 22:14:46 +0900

    feat: main関数でギブスサンプリングの作成途中とmakefileの修正

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include<iostream>
+#include<tuple>
 
 #include<rbm.h>
+#include<sampling.h>
+#include<data.h>
 
 
 
 int main(void){
-  rbm::Model calc_rbm(2, 2);
+  rbm::Model calc_rbm(4, 2);
 
   std::cout << "Hello World" << std::endl;
   std::cout << calc_rbm.visible_dim << " " <<  calc_rbm.hidden_dim << std::endl;
+
+  rbm::Sampling sampler;
+
+  rbm::DataSet sample = sampler.create_data_set(calc_rbm, 5, 5);
+  for(const auto& item: sample){
+    std::cout<<item.transpose() << std::endl;
+  }
+  // TODO: ギブスサンプリングの成功かどうかの実装確認
 }
